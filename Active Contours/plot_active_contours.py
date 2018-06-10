aa0e529e3c4c65092913da54ee1cfd331c21abf8
AKASH2907 <kmr2907akash@gmail.com>
2018-06-10 18:50:44 +0530

    adaptive contour

---
+++ Active Contours/plot_active_contours.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+from skimage import data
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+from cv2 import imread
+import scipy
+
+# split_version = scipy.__version__.split('.')
+# if not(split_version[-1].isdigit()): # Remove dev string if present
+#         split_version.pop()
+# scipy_version = list(map(int, split_version))
+# new_scipy = scipy_version[0] > 0 or \
+#             (scipy_version[0] == 0 and scipy_version[1] >= 14)
+
+img = imread('bilate.png')
+img = rgb2gray(img)
+
+s = np.linspace(0, 2*np.pi, 1000)
+x = 83 + 100*np.cos(s)
+y = 33 + 100*np.sin(s)
+init = np.array([x, y]).T
+print (init)
+'''
+alpha Higher values make snake contract faster
+beta Highervalue make snake smoother
+Gamma Timestepping parameter
+'''
+snake = active_contour(img, init, alpha=-1, beta=7, gamma=0.001, max_iterations=500)
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(111)
+plt.gray()
+ax.imshow(img)
+ax.plot(init[:, 0], init[:, 1], '--r', lw=3)
+ax.plot(snake[:, 0], snake[:, 1], '-b', lw=3)
+# ax.set_xticks([]), ax.set_yticks([])
+ax.axis([0, img.shape[1], img.shape[0], 0])
+# plt.imshow(img, cmap='gray')
+plt.show()
